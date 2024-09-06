fbd9dd6ddcfa1534d662aae840fae33b282218fd
Teerapat Kirdpaiboon <teerapat.kir@dome.tu.ac.th>
2024-09-06 15:12:28 +0700

    Update circle area calculation

--- app/src/main/java/com/example/eventlistener3/MainActivity.java
+++ app/src/main/java/com/example/eventlistener3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.eventlistener3;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
                     radius_input.setText("");
                 }
                 return false;
+            }
+        });
+
+        final Button calculate_btn = findViewById(R.id.calculate_btn);
+        calculate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double r_square = 0.0;
+                final DecimalFormat df = new DecimalFormat("0.00");
+                if(radius_input.getText().toString().isEmpty()){
+                    r_square = 0.0;
+                } else {
+                    r_square = Float.parseFloat((radius_input.getText().toString()));
+                }
+                output_text.setText(df.format(Math.PI*r_square));
             }
         });
     }
