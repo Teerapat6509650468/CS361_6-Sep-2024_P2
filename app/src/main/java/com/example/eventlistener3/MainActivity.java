23e4990d7e34ddd115fba4713085562cc69c2897
Teerapat Kirdpaiboon <teerapat.kir@dome.tu.ac.th>
2024-09-06 14:59:07 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/eventlistener3/MainActivity.java
+++ app/src/main/java/com/example/eventlistener3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.eventlistener3;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        final TextView output_text = findViewById(R.id.output_text);
+        final EditText radius_input = findViewById(R.id.radius);
+        radius_input.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                    output_text.setText(String.format("%s %s", output_text.getText(), radius_input.getText()));
+                    radius_input.setText("");
+                }
+                return false;
+            }
         });
     }
 }
